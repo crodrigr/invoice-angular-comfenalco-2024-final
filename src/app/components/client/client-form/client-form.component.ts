ba86708cc29c2cd0e73cd44fc551338d174d423e
crodrigr <crodrigr@gmail.com>
2024-08-09 20:18:48 -0500

    sweetalert2

--- src/app/components/client/client-form/client-form.component.ts
+++ src/app/components/client/client-form/client-form.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Client } from '../../../models/client';
 import { ClientViewComponent } from '../../invoice/client-view/client-view.component';
 import { FormsModule,NgForm } from '@angular/forms';
-import { ActivatedRoute, Route } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ClientService } from '../../../services/client.service';
+import Swal from 'sweetalert2';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -18,7 +20,8 @@ export class ClientFormComponent implements OnInit {
   client: Client;
 
   constructor(private clienteService: ClientService
-             ,private activatedRoute: ActivatedRoute) 
+             ,private activatedRoute: ActivatedRoute
+             ,private router: Router) 
   {
      this.client=new Client();
   }
@@ -32,20 +35,38 @@ export class ClientFormComponent implements OnInit {
     });
   }
 
-  onSubmit(clientForm: NgForm): void{
-
+  onSubmit(clientForm: NgForm): void{  
     if(clientForm.valid){
-
-      const clientObservable=this.client.id>0
-       ? this.clienteService.updateCliente(this.client) 
-       : this.clienteService.createCliente(this.client);
-      
+      let label='creó';
+      let clientObservable=new Observable<Client>();      
+      if(this.client.id>0){
+        clientObservable=this.clienteService.updateCliente(this.client);
+        label='actulizó'; 
+      }else{
+        clientObservable=this.clienteService.createCliente(this.client);
+      }      
       clientObservable.subscribe({
          next:(client)=>{
            this.client=client;
+           Swal.fire({
+            position: "top",
+            icon: "success",
+            title: `Se ${label} correctamente`,
+            showConfirmButton: true,
+            timer: 1600
+          });
+           this.router.navigate(['/client']);
          },
          error:(error)=>{
            console.log("Erro create cliente",error);
+           Swal.fire({
+            position: "top",
+            icon: "error",
+            title: `Erro create cliente`,
+            showConfirmButton: true,
+            timer: 1600
+          });
+
          }
 
       }) 
--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import {ClientService} from '../../services/client.service';
 import { Client } from '../../models/client';
 import { CommonModule } from '@angular/common';
-import { Route, RouterLink } from '@angular/router';
+import { Route, RouterModule } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-client',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './client.component.html',
   styleUrl: './client.component.css'
 })
@@ -41,6 +42,32 @@ export class ClientComponent implements OnInit{
      });
   }
 
-
+  removeCliente(client:Client):void{
+    Swal.fire({
+      title: "Está seguro?",
+      text: `¿Seguro que desea eliminar el cliente: ${client.nombre} ${client.apellido} `,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, eliminar"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.clientService.removeCliente(client.id).subscribe({
+          next:()=>{
+             this.clients=this.clients.filter(cli=> cli.id !== client.id);
+             Swal.fire({
+              title: "Cliente eliminado!",
+              text: `Cliente ${client.nombre} ${client.apellido}`,
+              icon: "success"
+            });
+          },
+          error: (err)=>{
+            console.error('Error al eliminar el cliente:',err);
+          }
+       });        
+      }
+    });
+  }
 
 }
